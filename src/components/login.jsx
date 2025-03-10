96d2e724a2ca20b027e803edc5c13f9ea032094d
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-10 06:17:40 +0500

    Update login.jsx

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -12,7 +12,7 @@ const Login = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/api/auth/login", {
+      const response = await fetch("https://inotebook-backenda.vercel.app/api/auth/login", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
